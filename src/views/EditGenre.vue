f84afac062edc65b3fea61b668bc116431d0eefa
Chintan Shah <chintan@wohlig.com>
2018-12-27 20:17:34 +0530

    ok

---
+++ src/views/EditGenre.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import Api from "@/services/apiServices";
+export default {
+  name: "home",
+  data() {
+    return {
+      genre: {}
+    };
+  },
+  created() {
+    this.getGenre();
+  },
+  methods: {
+    getGenre() {
+      Api.getGenre(this.$route.params.id).then(data => {
+        this.genres = data;
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
